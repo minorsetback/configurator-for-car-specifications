537eef7d7fa1e56c9ab736aaacf4352ad9649c09
minorsetback <vlad232362@gmail.com>
2022-11-01 15:31:52 +0200

    add redux

--- src/utility/types.ts
+++ src/utility/types.ts
@@ -2,16 +2,3 @@ export interface Config {
     name: string,
     type: string
 }
-
-export interface Data {
-    [key: string]: string | boolean;
-    air_suspension: boolean,
-    color: string, 
-    engine:string, 
-    id:string, 
-    material:string, 
-    name:string, 
-    signature:string, 
-    wheel:string, 
-    wheel_type:string, 
-}
